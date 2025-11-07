41cab01817f3bd78805d4babbb3d1557df4f14e6
sohaila-cu <sohailaharoun@cmail.carleton.ca>
2025-11-07 14:27:58 -0500

    changed names to include student IDs

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ ! -d "bin" ]; then
 else
 	rm bin/*
 fi
-g++ -g -O0 -I . -o bin/interrupts interrupts.cpp
+g++ -g -O0 -I . -o bin/interrupts.exe interrupts_101302936_101297624.cpp
--- interrupts_101302936_101297624.cpp
+++ interrupts_101302936_101297624.cpp
@@ -7,7 +7,7 @@
  *
  */
 
-#include"interrupts.hpp"
+#include"interrupts_101302936_101297624.hpp"
 
 std::tuple<std::string, std::string, int> simulate_trace(std::vector<std::string> trace_file, int time, std::vector<std::string> vectors, std::vector<int> delays, std::vector<external_file> external_files, PCB current, std::vector<PCB> wait_queue) {
 
